27b77bcfefaac83ebd3da59fb2d763509ce53f99
Caitlin Fabian <caitlinfstar@knights.ucf.edu>
2023-01-09 11:48:56 -0500

    Projects map

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -7,6 +7,7 @@ import * as RiIcons from 'react-icons/ri';
 import { GiPassport } from 'react-icons/gi';
 
 import '../scss/custom.scss';
+import { projectData } from './projectData';
 
 function ProjectsSection() {
   return (
@@ -15,77 +16,40 @@ function ProjectsSection() {
 
       <Container>
         <Row>
-          <Col className="d-flex justify-content-center">
-            <Card style={{ width: '18rem' }}>
-              <FaIcons.FaDumbbell
-                className="w-100"
-                size="30%"
-                style={{ padding: '20px' }}
-              />
-              <Card.Body>
-                <Card.Title>My Fitness Friend</Card.Title>
-                <Card.Text>
-                  Mobile App written in React Native. Worked in a team of 3
-                  members over the span of 2 months. Featured weight tracking
-                  with a graph, capabilities to store workouts, and ability to
-                  edit a user profile and store said data. Included firebase as
-                  a database.
-                </Card.Text>
-                <a
-                  href="https://github.com/Caitlin-Fabian/MyFitnessFriend"
-                  className="button"
+          {projectData.map((item) => {
+            return (
+              <>
+                <Col
+                  key={item.Title}
+                  className="d-flex justify-content-center"
                 >
-                  Github
-                </a>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col className="d-flex justify-content-center">
-            <Card style={{ width: '18rem' }}>
-              <GiPassport
-                className="w-100"
-                style={{ padding: '20px' }}
-                size="40%"
-              />
-              <Card.Body>
-                <Card.Title>Seeking Citizenship</Card.Title>
-                <Card.Text>
-                  Web application simplifying the naturalization process to the
-                  U.S. for inquiring immigrants. Using HTML, CSS, and Bootstrap.
-                  I worked in a team of 4 over the span of 48 hours to create
-                  this website.
-                </Card.Text>
-                <a href="https://github.com/Caitlin-Fabian/immigration-app">
-                  Github
-                </a>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col className="d-flex justify-content-center">
-            <Card style={{ width: '18rem' }}>
-              <RiIcons.RiSurveyLine
-                className="w-100"
-                size="30%"
-                style={{ padding: '20px' }}
-              />
-              <Card.Body>
-                <Card.Title>Survey Studies</Card.Title>
-                <Card.Text>
-                  Web application that allows individuals to create,
-                  participate, and track their surveys. Worked in a team of 4
-                  members over the span of 2 weeks. Used ReactJS and Bootstrap
-                  for front end, ExpressJs for backend , and Digital Ocean for
-                  database.
-                </Card.Text>
-                <a
-                  href="https://github.com/Caitlin-Fabian/MyFitnessFriend"
-                  className="button"
-                >
-                  Github
-                </a>
-              </Card.Body>
-            </Card>
-          </Col>
+                  <Card style={{ width: '18rem' }}>
+                    <span
+                      className="w-100"
+                      style={{
+                        padding: '20px',
+                        width: '100px',
+                        height: ' 200px',
+                      }}
+                    >
+                      {item.icon}
+                    </span>
+
+                    <Card.Body>
+                      <Card.Title>{item.title}</Card.Title>
+                      <Card.Text>{item.description}</Card.Text>
+                      <a
+                        href={item.link}
+                        className="button"
+                      >
+                        Github
+                      </a>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </>
+            );
+          })}
         </Row>
       </Container>
     </div>
---
+++ src/components/projectData.js
@@ -0,0 +1,27 @@
+import * as FaIcons from 'react-icons/fa';
+import * as RiIcons from 'react-icons/ri';
+import { GiPassport } from 'react-icons/gi';
+
+export const projectData = [
+  {
+    title: 'My Fitness Friend',
+    icon: <FaIcons.FaDumbbell size="8x" />,
+    description:
+      'Mobile App written in React Native. Worked in a team of 3 members over the span of 2 months. Featured weight trackingwith a graph, capabilities to store workouts, and ability toedit a user profile and store said data. Included firebase as a database.',
+    link: 'https://github.com/Caitlin-Fabian/MyFitnessFriend',
+  },
+  {
+    title: 'Seeking Citizenship',
+    icon: <GiPassport size="8x" />,
+    description:
+      'Web application simplifying the naturalization process to the U.S. for inquiring immigrants. Using HTML, CSS, and Bootstrap. I worked in a team of 4 over the span of 48 hours to create this website.',
+    link: 'https://github.com/Caitlin-Fabian/immigration-app',
+  },
+  {
+    title: 'Survey Studies',
+    icon: <RiIcons.RiSurveyLine size="8x" />,
+    description:
+      'Web application that allows individuals to create, participate, and track their surveys. Worked in a team of 4 members over the span of 2 weeks. Used ReactJS and Bootstrap for front end, ExpressJs for backend , and Digital Ocean for database.',
+    link: 'https://github.com/Caitlin-Fabian/immigration-app',
+  },
+];
